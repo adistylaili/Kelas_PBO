6acc7846786c73f0cda6f5f547c4cd7c9c1bc54a
adistylaili <adistylaili9446@gmail.com>
2021-02-26 02:51:11 -0800

    Bismillah

---
+++ aktifitas1.py
@@ -0,0 +1,12 @@
+#INPUT
+
+nama=input("masukan nama Anda: ")
+umur=20
+
+#OUTPUT
+print("")
+print("====INPUT DAN OUTPUT PADA BAHASA PEMOGRAMAN PYTHON====")
+print("nama Anda: ",nama)
+print("umur Anda: ",umur)
+print("")
+
